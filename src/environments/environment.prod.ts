15796fd182123b187a179b2d49d49bf4071f7038
Hoang Phan <hoang@netengine.com.au>
2021-12-11 17:37:28 +0700

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBbVUR69W4qn89miqSkz08bwHqk324wMFw",
+    authDomain: "kubbyblog.firebaseapp.com",
+    projectId: "kubbyblog",
+    storageBucket: "kubbyblog.appspot.com",
+    messagingSenderId: "627853736865",
+    appId: "1:627853736865:web:f58d075ac2cebd275e29e0"
+  },
 };
